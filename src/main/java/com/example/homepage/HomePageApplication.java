ed435e5afd84d812d505c69f83b495d12d24bdb4
31augChou <erix4556@gmail.com>
2026-03-07 01:33:16 +0700

    remove errors

--- src/main/java/com/example/homepage/HomePageApplication.java
+++ src/main/java/com/example/homepage/HomePageApplication.java
@@ -2,7 +2,6 @@ package com.example.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,8 +11,4 @@ public class HomePageApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomePageApplication.class, args);
     }
-@GetMapping
-    public String homePage() {
-        return "Hello World"+ "\u001B";
-}
 }
