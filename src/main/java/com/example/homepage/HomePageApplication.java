6c1f07e59ba41a6f2df1b82a4f6099efa37f5585
31augChou <erix4556@gmail.com>
2026-03-06 23:14:01 +0700

    Homepage file

--- src/main/java/com/example/homepage/HomePageApplication.java
+++ src/main/java/com/example/homepage/HomePageApplication.java
@@ -2,13 +2,18 @@ package com.example.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HomePageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HomePageApplication.class, args);
     }
-
+@GetMapping
+    public String homePage() {
+        return "Hello World"+ "\u001B";
+}
 }
